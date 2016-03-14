31ed34c3050326273a7c5d210310ec7046680f96
Andrew Chiw <randomshinichi4869@gmail.com>
2016-03-14 16:27:30 +0100

    Lockin Amplifier using Integer Math

--- auteur.ino
+++ auteur.ino
@@ -10,8 +10,10 @@ double averager[averager_size]; // the averaging array
 unsigned int averager_i; // keeps track of where we are in the averager array
 unsigned int averager_counter; // counts how many times the averager has been added to
 double averager_sum; // constantly updated sum of averaging array
-double minDACvolt=0.5521215820;
-double maxDACvolt=2.7575463867;
+int minADC = 688; // these are reasonable defaults, will be changed with calibration
+int maxADC = 3424;
+double minDACvolt = 0.5518798828;
+double maxDACvolt = 2.7585937500;
 boolean lockamp_debug = false;
 boolean averager_debug = false;
 boolean results_display = false;
@@ -20,12 +22,14 @@ void setup() {
   analogWriteResolution(12);
   analogReadResolution(12);
   pinMode(A0, INPUT);
-  Serial.begin(115200);
-  Serial.println("Available commands: lockamp(frequency,repeat) analogread analogwrite(0-4095) calibrate status debug(lockamp_debug, averager_debug, results_display)");
-  Serial.print("auteur is listening: ");
+  while (!SerialUSB);
+  SerialUSB.begin(115200);
+  SerialUSB.println("Available commands: lockamp(frequency,repeat) analogread analogwrite(0-4095) calibrate status debug(lockamp_debug, averager_debug, results_display)");
+  SerialUSB.print("auteur is listening: ");
   inputstring.reserve(200); // Shell accepts up to 200 characters, 10 arguments
 }
 void loop() {
+  if (SerialUSB.available())serialEvent(); // to enable input via native USB port, not needed if just using Serial/Programming port
   // put your main code here, to run repeatedly:
   if (stringcomplete) {
     shell();
@@ -38,7 +42,7 @@ void loop() {
   }
 }
 void shell() {
-  Serial.println(inputstring);
+  SerialUSB.println(inputstring);
   String *inputstring_processed;
   inputstring_processed = splitcmd(inputstring, ' ');
   if (inputstring_processed[0] == "lockamp") {
@@ -46,23 +50,23 @@ void shell() {
     param1 = string2int(inputstring_processed[1]);
     param2 = string2int(inputstring_processed[2]);
     lockinamplifier(param1, param2);
-    Serial.print("averager_counter (ignore result below if this is <10,000): ");Serial.println(averager_counter);
-    double averager_result = 2*averager_sum/averager_size;
-    Serial.print("averager_result: ");Serial.println(averager_result,10);
+    SerialUSB.print("averager_counter (ignore result below if this is <10,000): "); SerialUSB.println(averager_counter);
+    double averager_result = 2 * averager_sum / averager_size;
+    SerialUSB.print("averager_result: "); SerialUSB.println(averager_result, 10);
   }
   if (inputstring_processed[0] == "debug") {
     int param1 = string2int(inputstring_processed[1]);
     int param2 = string2int(inputstring_processed[2]);
     int param3 = string2int(inputstring_processed[3]);
-    if(param1)
+    if (param1)
       lockamp_debug = true;
     else
       lockamp_debug = false;
-    if(param2)
+    if (param2)
       averager_debug = true;
     else
       averager_debug = false;
-    if(param3)
+    if (param3)
       results_display = true;
     else
       results_display = false;
@@ -70,31 +74,37 @@ void shell() {
   if (inputstring_processed[0] == "analogread") {
     int input = analogRead(ADC0);
     double translation = int2voltage(input, 0, 3.3);
-    Serial.print(input); Serial.print(" "); Serial.print(translation,3); Serial.println("V");
+    SerialUSB.print(input); SerialUSB.print(" "); SerialUSB.print(translation, 3); SerialUSB.println("V");
   }
   if (inputstring_processed[0] == "analogwrite") {
     int param1 = string2int(inputstring_processed[1]);
-    analogWrite(DAC0,param1);
-    analogWrite(DAC1,param1);
+    analogWrite(DAC0, param1);
+    analogWrite(DAC1, param1);
   }
   if (inputstring_processed[0] == "calibrate") {
     calibrate();
   }
   if (inputstring_processed[0] == "status") {
-    Serial.print("minDACvolt: ");Serial.print(minDACvolt,10);Serial.print(" maxDACvolt: ");Serial.println(maxDACvolt,10);
-    Serial.print("lockamp_debug: ");Serial.print(lockamp_debug);Serial.print(" averager_debug: ");Serial.print(averager_debug);Serial.print(" results_display: ");Serial.println(results_display);
-    Serial.print("averager_counter (ignore result below if this is <10,000): ");Serial.println(averager_counter);
-    double averager_result = 2*averager_sum/averager_size;
-    Serial.print("averager_result: ");Serial.println(averager_result,10);
-    Serial.print("averager (1st 100): ");printthisarray(100, averager);
+    SerialUSB.print("minADC: "); SerialUSB.print(minADC); SerialUSB.print(" maxADC: "); SerialUSB.println(maxADC);
+
+    starttime = micros();
+    SerialUSB.print("minDACvolt: "); SerialUSB.print(minDACvolt, 10); SerialUSB.print(" maxDACvolt: "); SerialUSB.println(maxDACvolt, 10);
+    elapsedtime = micros() - starttime;
+    SerialUSB.print(elapsedtime); SerialUSB.println(" micros taken");
+
+    SerialUSB.print("lockamp_debug: "); SerialUSB.print(lockamp_debug); SerialUSB.print(" averager_debug: "); SerialUSB.print(averager_debug); SerialUSB.print(" results_display: "); SerialUSB.println(results_display);
+    SerialUSB.print("averager_counter (ignore result below if this is <10,000): "); SerialUSB.println(averager_counter);
+    double averager_result = 2 * averager_sum / averager_size;
+    SerialUSB.print("averager_result: "); SerialUSB.println(averager_result, 10);
+    SerialUSB.print("averager (1st 100): "); printthisarray(100, averager);
   }
-  Serial.print("auteur > ");
+  SerialUSB.print("auteur > ");
 }
 
 void serialEvent() {
-  while (Serial.available()) {
+  while (SerialUSB.available()) {
     // get the new byte:
-    char inChar = (char)Serial.read();
+    char inChar = (char)SerialUSB.read();
     // add it to the inputString:
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
--- helperfunctions.ino
+++ helperfunctions.ino
@@ -3,17 +3,21 @@ void lockinamplifier(int rate, int repeat) {
   averager_counter = 0;
   averager_i = 0;
   averager_sum = 0;
-  int delaymicros = 1000000 / (rate * wave_samples);
+  int delaymicros = (1000000 / (rate * wave_samples))-39; // compensate for execution time of output_readin() algo
+  SerialUSB.print("delaymicros: ");  SerialUSB.println(delaymicros);
   if (repeat > 0) {
     while (repeat > 0) {
+      starttime=micros();
       output_readin(delaymicros);
+      elapsedtime=micros()-starttime;
+      SerialUSB.print(elapsedtime);SerialUSB.println(" micros taken by output_readin");
       repeat--;
       if (results_display) {
         {
           double result = averager_sum / averager_size;
-          Serial.print("results: ");  //ensure you have >10000 samples in averager to make a meaningful reading
-          Serial.println(averager_sum / averager_size, 10);
-          Serial.print("ave_voltage: "); Serial.println(sinescale2voltage(result), 10);
+          SerialUSB.print("results: ");  //ensure you have >10000 samples in averager to make a meaningful reading
+          SerialUSB.println(averager_sum / averager_size, 10);
+          SerialUSB.print("ave_voltage: "); SerialUSB.println(sinescale2voltage(result), 10);
         }
       }
     }
@@ -24,9 +28,9 @@ void lockinamplifier(int rate, int repeat) {
       if (results_display) {
         {
           double result = averager_sum / averager_size;
-          Serial.print("results: ");  //ensure you have >10000 samples in averager to make a meaningful reading
-          Serial.println(averager_sum / averager_size, 10);
-          Serial.print("ave_voltage: "); Serial.println(sinescale2voltage(result), 10);
+          SerialUSB.print("results: ");  //ensure you have >10000 samples in averager to make a meaningful reading
+          SerialUSB.println(averager_sum / averager_size, 10);
+          SerialUSB.print("ave_voltage: "); SerialUSB.println(sinescale2voltage(result), 10);
         }
       }
     }
@@ -39,24 +43,23 @@ void lockinamplifier(int rate, int repeat) {
 void output_readin(int delaymicros) {
   for (int i = 0; i < wave_samples; ++i) {
     analogWrite(DAC1, scaledsine[i]);
-    delayMicroseconds(delaymicros - 51); //executing the following lines will take ~51micros
+    //delayMicroseconds(delaymicros);
+    //delayMicroseconds itself takes about 2 micros, so when benchmarking output_readin, add 2micros to the result (37+2=39)
     int input = analogRead(ADC0); //this will be between 0-4096, corresponding to 0 and 3.3V
-    double input_voltage = int2voltage(input, 0, 3.3);
-    double output_voltage = int2voltage(scaledsine[i], minDACvolt, maxDACvolt);
-    output_voltage += minDACvolt; // int2voltage above this shifted the whole value down by 0.553, correcting this back up
-    double answer = voltage2sinescale(input_voltage) * voltage2sinescale(output_voltage);
+    double answer = int2sinescale(input, minADC, maxADC) * int2sinescale(scaledsine[i], 0, 4095);
     add2averager(answer);
-
     if (lockamp_debug) {
-      Serial.print("i: "); Serial.print(i); Serial.print(" scaledsine[i]: "); Serial.println(scaledsine[i]);
-      Serial.print("I put "); Serial.print(output_voltage, 10);Serial.print("V out, read back: "); Serial.println(input_voltage, 10); 
+      double output_voltage = int2voltage(scaledsine[i], minDACvolt, maxDACvolt);
+      double input_voltage = int2voltage(input, 0, 3.3);
+      SerialUSB.print("i: "); SerialUSB.print(i); SerialUSB.print(" scaledsine[i]: "); SerialUSB.println(scaledsine[i]);
+      SerialUSB.print("I put "); SerialUSB.print(output_voltage, 10); SerialUSB.print("V out, read back: "); SerialUSB.println(input_voltage, 10);
     }
   }
 }
-void add2averager(double Voutput) {
+inline void add2averager(double Voutput) {
   static double oldvalue = 0;
   if (averager_debug) {
-    Serial.print("averager_i: "); Serial.println(averager_i);
+    SerialUSB.print("averager_i: "); SerialUSB.println(averager_i);
   }
   if (averager_i == averager_size) {
     averager_i = 0;
@@ -68,30 +71,35 @@ void add2averager(double Voutput) {
   averager_i++;
   averager_counter++;
 }
-double voltage2sinescale(double input) {
-  double range = maxDACvolt - minDACvolt;
-  input = input - (range / 2) - minDACvolt; // move 0.553-2.759V curve down to have its center on the x axis
-  return 2 * input / range; // scale it to 1
-}
 double sinescale2voltage(double input) {
-  input = input * 1.65;
-  return input + 1.65;
+  return (input * 1.65) + 1.65;
 }
 double int2voltage(int input, double vlow, double vhigh) { // voltage = int2voltage(800,0.55,2.75)
   double granularity = (vhigh - vlow) / 4096;
-  return granularity * input;
+  granularity = (granularity * input) + vlow;
+  return granularity;
+}
+inline double int2sinescale(int input, int minimum, int maximum) {
+  double sinescale;
+  int range = (maximum - minimum) / 2;
+  input = input - range - minimum;
+  sinescale = double(input) / double(range);
+  return sinescale;
 }
 void calibrate() {
-  minDACvolt = calibrate_helper(0);
-  maxDACvolt = calibrate_helper(4095);
-  Serial.print(minDACvolt, 10); Serial.print(" "); Serial.println(maxDACvolt, 10);
+  minADC = calibrate_helper(0);
+  maxADC = calibrate_helper(4095);
+  SerialUSB.print("minADC: "); SerialUSB.print(minADC); SerialUSB.print(" maxADC: "); SerialUSB.println(maxADC);
+  minDACvolt = int2voltage(minADC, 0, 3.3);
+  maxDACvolt = int2voltage(maxADC, 0, 3.3);
+  SerialUSB.print("minDACvolt: "); SerialUSB.print(minDACvolt, 10); SerialUSB.print(" maxDACvolt: "); SerialUSB.println(maxDACvolt, 10);
 }
-double calibrate_helper(int x) {
+int calibrate_helper(int x) {
   analogWrite(DAC1, x);
-  double calib_accum = 0;
+  int calib_accum = 0;
   int i = 40;
   while (i > 0) {
-    calib_accum += int2voltage(analogRead(ADC0), 0, 3.3);
+    calib_accum += analogRead(ADC0);
     delay(200);
     i--;
   }
@@ -105,10 +113,10 @@ void zerothisarray(int size, double thearray[]) {
 }
 void printthisarray(int size, double thearray[]) {
   for (int i = 0; i < size; i++) {
-    Serial.print(thearray[i], 5);
-    Serial.print(",");
+    SerialUSB.print(thearray[i], 5);
+    SerialUSB.print(",");
   }
-  Serial.println("");
+  SerialUSB.println("");
 }
 String* splitcmd(String command, char delimiter) {
   int endposition = -1;
